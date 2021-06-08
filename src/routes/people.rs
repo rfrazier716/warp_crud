ee128ced4014ca0080721fcffd304d4f9d47d379
rfrazier <rfrazier716@gmail.com>
2021-06-08 17:04:51 -0400

    added "end" flag to people route

--- src/routes/people.rs
+++ src/routes/people.rs
@@ -1,6 +1,7 @@
 use super::*;
 use crate::error::ServerError::DataBaseError;
 use mongodb::bson::doc;
+use warp::filters::path;
 use warp::{reject, Filter, Rejection, Reply};
 
 pub fn people_routes(
@@ -11,20 +12,13 @@ pub fn people_routes(
         .and(warp::get())
         .and(with_db(client.clone()))
         .and(
-            warp::path::param::<String>()
-                .or(warp::any().map(|| String::default()))
+            path::param::<String>()
+                .or(warp::any().map(|| String::from("")))
                 .unify(),
         )
+        .and(path::end())
         .and_then(get_people)
 }
-
-// pub async fn read_people(client: db::Client) -> Result<impl Reply, Rejection> {
-//     tracing::span!(tracing::Level::INFO, "getting all people");
-//     let reply = db::get_people(&client)
-//         .await
-//         .map_err(|source| error::ServerError::DataBaseError { source })?;
-//     Ok(warp::reply::json(&reply))
-// }
 
 pub async fn get_people<T>(client: db::Client, name_str: T) -> Result<impl Reply, Rejection>
 where
@@ -41,20 +35,6 @@ where
 
     let reply = db::get_people(&client, request_filter)
         .await
-        .map_err(|source| error::ServerError::DataBaseError { source })?;
+        .map_err(|source| DataBaseError { source })?;
     Ok(warp::reply::json(&reply))
-}
-pub async fn read_person<T>(client: db::Client, name_str: T) -> Result<impl Reply, Rejection>
-where
-    T: AsRef<str>,
-{
-    tracing::span!(tracing::Level::INFO, "getting person");
-    if let Some(reply) = db::get_person(&client, name_str.as_ref())
-        .await
-        .map_err(|source| DataBaseError { source })?
-    {
-        Ok(warp::reply::json(&reply))
-    } else {
-        Err(warp::reject::not_found())
-    }
 }

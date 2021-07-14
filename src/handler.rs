4b8655e80dadbab480b8c92578d37a741fc5420a
rfrazier <rfrazier716@gmail.com>
2021-07-14 17:01:11 -0400

    added macro in handler processes errors and generates replies insead of rejections. rejections should be for filter errors instead.

--- src/handler.rs
+++ src/handler.rs
@@ -3,6 +3,38 @@ use warp::{Rejection, Reply};
 
 use crate::error::Error::*;
 use crate::{data, db};
+use std::convert::Infallible;
+
+
+macro_rules! warp_handle {
+    // handles errors generated during the handler that should generate http responses
+    ($result:expr) => {
+        match $result {
+            Ok(x) => x,
+            Err(error) => return recover(error),
+        }
+    };
+}
+
+pub fn recover(error: crate::error::Error) -> Result<Box<dyn Reply>, Infallible> {
+    match error {
+        MongoOidError(oid) => Ok(Box::new(
+            warp::http::Response::builder()
+                .status(400)
+                .body(format!("{}", oid)),
+        )),
+        MongoQueryError(mongo_error) => Ok(Box::new(
+            warp::http::Response::builder()
+                .status(500)
+                .body(format!("{}", mongo_error)),
+        )),
+        _ => Ok(Box::new(
+            warp::http::Response::builder()
+                .status(500)
+                .body("500: Unhandled Exception"),
+        )),
+    }
+}
 
 pub async fn health(client: db::Client) -> Result<impl Reply, Rejection> {
     tracing::info!("Pinging Database");
@@ -11,6 +43,7 @@ pub async fn health(client: db::Client) -> Result<impl Reply, Rejection> {
 }
 
 pub mod people {
+
     use super::*;
 
     pub async fn create(
@@ -26,28 +59,19 @@ pub mod people {
         Ok(warp::reply::json(&reply))
     }
 
-    pub async fn read_single<T>(client: db::Client, user_id: T) -> Result<Box<dyn Reply>, Rejection>
+    pub async fn read_single<T>(client: db::Client, user_id: T) -> Result<Box<dyn Reply>, Infallible>
     where
         T: AsRef<str>,
     {
-        let reply = db::get_person(&client, user_id.as_ref()).await;
-
-        // if it's an OID conversion error we want that to show up as a generic 404
-        let result = match reply {
-            Err(MongoOidError(_)) => None,
-            other => other?,
-        };
-
-        // Implement the result, with a success or 404 error
-        if let Some(person) = result {
+        if let Some(person) = warp_handle!(db::get_person(&client, user_id.as_ref()).await) {
             Ok(Box::new(warp::reply::json(&person)))
         } else {
             Ok(Box::new(
                 warp::http::Response::builder()
                     .status(404)
-                    .body("404: Person not found"),
+                    .body("404: Not Found"),
             ))
-        } // if there wasn't a person with that ID return a 404 error
+        }
     }
 
     pub async fn update<T>(

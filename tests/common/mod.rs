6f6b555bc11df83bab80ea202102ca4f39dda5a1
Ryan Frazier <Ryan@Fotonixx.com>
2021-08-30 17:18:05 -0400

    Added integration tests for todo endpoints. Deleted People Endpoints

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -9,3 +9,32 @@ pub async fn spawn_app() -> Result<SocketAddr> {
     tokio::task::spawn(server);
     Ok(addr) // Return the bound address
 }
+
+pub struct App {
+    address: std::net::SocketAddr,
+}
+
+impl App {
+    pub async fn launch(run_environment: Option<&str>) -> Result<App> {
+        let env = match run_environment {
+            Some(env) => env,
+            None => "Test",
+        };
+
+        // Set the environment so the right config is loaded
+        std::env::set_var("RUN_ENV", env);
+        let app_settings = config::Settings::new()?;
+        let (addr, server) = startup::run(app_settings).await?;
+        tokio::task::spawn(server);
+        Ok(App { address: addr })
+    }
+    
+    pub fn route(self, endpoint: &str) -> String {
+        format!(
+            "http://{}:{}{}",
+            self.address.ip(),
+            self.address.port(),
+            endpoint
+        )
+    }
+}
--- tests/test_todo_endpoints.rs
+++ tests/test_todo_endpoints.rs
@@ -5,18 +5,11 @@ use warp_crud::data;
 async fn test_getting_todos() {
     //spawn the app so the server is running
     //need to block on this or the request can happen before the server starts
-    let app_address = tokio::join!(common::spawn_app()).0.unwrap();
+
+    let app = common::App::launch(Some("Test")).await.unwrap();
     let client = reqwest::Client::new();
 
-    // Create the health endpoint and set a get request to the health endpoint
-    let endpoint = format!(
-        "http://{}:{}/api/todos",
-        app_address.ip(),
-        app_address.port()
-    );
-
-    //Build a response
-    let resp = client.get(endpoint).send().await.unwrap();
+    let resp = client.get(app.route("/api/todos")).send().await.unwrap();
 
     // Verify we got a success
     assert!(resp.status().is_success());
@@ -26,32 +19,91 @@ async fn test_getting_todos() {
     // assure that the body has a length of 1 and that the only element says the default message
     assert_eq!(body.len(), 1);
     assert_eq!(body[0].name, "Delete This Todo");
-
-
-    // assert that we got a "success" code back
 }
 
 // TODO: Figure out how to initialize a new cookie session
-// #[tokio::test]
-// async fn test_creating_todo() {
-//     //spawn the app so the server is running
-//     //need to block on this or the request can happen before the server starts
-//     let app_address = tokio::join!(common::spawn_app()).0.unwrap();
-//     let client = reqwest::Client::new();
+#[tokio::test]
+async fn test_creating_todo() {
+    //spawn the app so the server is running
+    //need to block on this or the request can happen before the server starts
+    let app = common::App::launch(Some("Test")).await.unwrap();
+    let endpoint = app.route("/api/todos");
+    let client = reqwest::Client::builder().cookie_store(true).build().expect("Could not Create Client");
 
-//     // Create the todos endpoint
-//     let endpoint = format!(
-//         "http://{}:{}/api/todos",
-//         app_address.ip(),
-//         app_address.port()
-//     );
 
-//     let new_todo = data::TodoRequest{name: "Run To The Hills!".to_owned()};
 
-//     //Build a response
-//     let resp = client.post(endpoint).json::<data::TodoRequest>(&new_todo).send().await.unwrap();
+    // Run a get reqest to the app so we get a session cookie back
+    client.get(&endpoint).send().await.expect("Error Running Get Request to App");
 
-//     // Verify we got a success
-//     assert!(resp.status().is_success());
+    let new_todo = data::TodoRequest{name: "Run To The Hills!".to_owned()};
 
-// }
+    // Send a post request to the
+    let resp = client.post(&endpoint).json::<data::TodoRequest>(&new_todo).send().await.unwrap();
+
+    // Verify we got a success
+    assert!(resp.status().is_success());
+
+    // Verify that if we now run a get request we should have 2 items, 
+    let resp = client.get(&endpoint).send().await.expect("Error Running Get Request to App");
+    let body = resp.json::<Vec<data::Todo>>().await.unwrap();
+
+    // assure that the body has a length of 1 and that the only element says the default message
+    assert_eq!(body.len(), 2);
+    assert_eq!(body[1].name, "Run To The Hills!");
+}
+
+#[tokio::test]
+async fn test_updating_todo() {
+    //spawn the app so the server is running
+    //need to block on this or the request can happen before the server starts
+    let app = common::App::launch(Some("Test")).await.unwrap();
+    let endpoint = app.route("/api/todos");
+    let client = reqwest::Client::builder().cookie_store(true).build().expect("Could not Create Client");
+
+
+
+    // Run a get reqest to the app so we get a session cookie back
+    let resp = client.get(&endpoint).send().await.expect("Error Running Get Request to App");
+    let todo_id = resp.json::<Vec<data::Todo>>().await.unwrap()[0].id;
+
+    // make a new todo request and send an update request with the todo_id endpoint
+    let new_todo = data::TodoRequest{name: "Run To The Hills!".to_owned()};
+    let resp = client.put(format!("{}/{}",endpoint, todo_id)).json::<data::TodoRequest>(&new_todo).send().await.unwrap();
+
+    // Verify we got a success
+    assert!(resp.status().is_success());
+
+    // Verify that if we now run a get request we get the updated item
+    let resp = client.get(&endpoint).send().await.expect("Error Running Get Request to App");
+    let body = resp.json::<Vec<data::Todo>>().await.unwrap();
+
+    // assure that the body has a length of 1 and that the only element says the default message
+    assert_eq!(body.len(), 1);
+    assert_eq!(body[0].name, "Run To The Hills!");
+}
+
+#[tokio::test]
+async fn test_deleting_todo() {
+    //spawn the app so the server is running
+    //need to block on this or the request can happen before the server starts
+    let app = common::App::launch(Some("Test")).await.unwrap();
+    let endpoint = app.route("/api/todos");
+    let client = reqwest::Client::builder().cookie_store(true).build().expect("Could not Create Client");
+
+    // Run a get reqest to the app so we get a session cookie back
+    let resp = client.get(&endpoint).send().await.expect("Error Running Get Request to App");
+    let todo_id = resp.json::<Vec<data::Todo>>().await.unwrap()[0].id;
+
+    // Send a Delete request containing the id of the todo we want to delete
+    let resp = client.delete(format!("{}/{}",endpoint, todo_id)).send().await.unwrap();
+
+    // Verify we got a success
+    assert!(resp.status().is_success());
+
+    // Verify that if we now run a get request we get an empty vector
+    let resp = client.get(&endpoint).send().await.expect("Error Running Get Request to App");
+    let body = resp.json::<Vec<data::Todo>>().await.unwrap();
+
+    // assure that the body has a length of 1 and that the only element says the default message
+    assert_eq!(body.len(), 0);
+}

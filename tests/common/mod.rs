7451111cf963c00f5da2fe661913d5fd95e3433d
Ryan Frazier <Ryan@Fotonixx.com>
2021-08-31 15:36:04 -0400

    added cfg test to common module

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,18 +2,12 @@ use std::net::SocketAddr;
 use tokio;
 use warp_crud::{config, error::Result, startup};
 
-pub async fn spawn_app() -> Result<SocketAddr> {
-    std::env::set_var("RUN_ENV", "Test"); //set the test environment so the right config is loaded
-    let app_settings = config::Settings::new()?;
-    let (addr, server) = startup::run(app_settings).await?;
-    tokio::task::spawn(server);
-    Ok(addr) // Return the bound address
-}
-
+#[cfg(test)]
 pub struct App {
-    address: std::net::SocketAddr,
+    address: SocketAddr,
 }
 
+#[cfg(test)]
 impl App {
     pub async fn launch(run_environment: Option<&str>) -> Result<App> {
         let env = match run_environment {
